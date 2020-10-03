767f5ebf56a6c64c050121850d52eb09c28a6239
mtokioka <iede.nyanko@gmail.com>
2020-10-03 13:10:59 +0900

    team 100

--- golang/cmd/xsuportal/main.go
+++ golang/cmd/xsuportal/main.go
@@ -38,7 +38,7 @@ import (
 )
 
 const (
-	TeamCapacity               = 50
+	TeamCapacity               = 100
 	AdminID                    = "admin"
 	AdminPassword              = "admin"
 	DebugContestStatusFilePath = "/tmp/XSUPORTAL_CONTEST_STATUS"
