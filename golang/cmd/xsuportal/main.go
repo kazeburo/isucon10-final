ca1530458a2c4ed2a9d69289a4ad78d54440fab1
Masahiro Nagano <kazeburo@gmail.com>
2020-10-03 11:37:08 +0900

    50

--- golang/cmd/xsuportal/main.go
+++ golang/cmd/xsuportal/main.go
@@ -36,7 +36,7 @@ import (
 )
 
 const (
-	TeamCapacity               = 10
+	TeamCapacity               = 50
 	AdminID                    = "admin"
 	AdminPassword              = "admin"
 	DebugContestStatusFilePath = "/tmp/XSUPORTAL_CONTEST_STATUS"
