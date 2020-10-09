0c85538a27567a56d813811ca2a5c5632bbac8ee
Masahiro Nagano <kazeburo@gmail.com>
2020-10-09 14:44:29 +0900

    fix

--- golang/cmd/benchmark_server/main.go
+++ golang/cmd/benchmark_server/main.go
@@ -212,9 +212,9 @@ func (b *benchmarkReportService) saveAsFinished(db sqlx.Execer, job *xsuportal.B
 	}
 	_, err = db.Exec("UPDATE `team_scores`"+
 		"SET "+
-		"`best_score` = IF(? >= `best_score`,?,`best_score`), "+
-		"`best_started_at` = IF(? >= `best_score`,?,`best_started_at`), "+
-		"`best_finished_at` = IF(? >= `best_score`,?,`best_finished_at`), "+
+		"`best_score` = IF(? >= IFNULL(`best_score`,0),?,`best_score`), "+
+		"`best_started_at` = IF(? >= IFNULL(`best_score`,0),?,`best_started_at`), "+
+		"`best_finished_at` = IF(? >= IFNULL(`best_score`,0),?,`best_finished_at`), "+
 		"`latest_score` = ?, "+
 		"`latest_started_at` = ?, "+
 		"`latest_finished_at` = ?, "+
